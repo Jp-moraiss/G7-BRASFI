26b662b631d9c87b1b3d5b3d8276cdef852ace37
Jp-moraiss <164018039+Jp-moraiss@users.noreply.github.com>
2025-05-04 01:11:41 -0300

    chat backend

--- src/main/java/com/g7/brasfi/chat/entities/Message.java
+++ src/main/java/com/g7/brasfi/chat/entities/Message.java
@@ -1,10 +1,13 @@
-package com.g7.brasfi.chat;
+package com.g7.brasfi.chat.entities;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -12,6 +15,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
@@ -20,10 +24,15 @@ import lombok.Setter;
 @EqualsAndHashCode(of = "id")
 public class Message {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.UUID)
-	private String id;
-	private String sender;
-	private String content;
-	private LocalDateTime time;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    
+    private String sender;
+    private String content;
+    private LocalDateTime timeStamp;
+    
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
 }
---
+++ src/main/java/com/g7/brasfi/chat/entities/Room.java
@@ -0,0 +1,37 @@
+package com.g7.brasfi.chat.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "rooms")
+@EqualsAndHashCode(of = "id")
+public class Room {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    
+    private String roomId;
+    
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Message> messages = new ArrayList<>();
+}
