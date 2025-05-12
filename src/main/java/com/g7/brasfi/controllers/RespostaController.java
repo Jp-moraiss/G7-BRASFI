db53082ba2bcdd3b8154d70c2931e919ae164b1c
henrique-gueiros <riquewg@gmail.com>
2025-05-12 17:42:15 -0300

    novos htmls e ajustes na estrutura do questionário para fazer os devidos testes

--- src/main/java/com/g7/brasfi/controllers/RespostaController.java
+++ src/main/java/com/g7/brasfi/controllers/RespostaController.java
@@ -2,6 +2,9 @@ package com.g7.brasfi.controllers;
 
 import com.g7.brasfi.domain.resposta.Resposta;
 import com.g7.brasfi.services.RespostaService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,5 +20,16 @@ public class RespostaController {
     public ResponseEntity<Resposta> cadastrarResposta(@RequestBody Resposta resposta) {
         Resposta salva = respostaService.salvar(resposta);
         return ResponseEntity.ok(salva);
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<Resposta>> listarTodas(){
+    	List<Resposta> respostas = respostaService.buscarTodas();
+    	
+    	 if (respostas.isEmpty()) {
+    	        return ResponseEntity.noContent().build();
+    	    } else {
+    	        return ResponseEntity.ok(respostas);
+    	    }
     }
 }
--- src/main/java/com/g7/brasfi/services/RespostaService.java
+++ src/main/java/com/g7/brasfi/services/RespostaService.java
@@ -2,6 +2,9 @@ package com.g7.brasfi.services;
 
 import com.g7.brasfi.domain.resposta.Resposta;
 import com.g7.brasfi.repositories.RespostaRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +16,9 @@ public class RespostaService {
 
     public Resposta salvar(Resposta resposta) {
         return respostaRepository.save(resposta);
+    }
+    
+    public List<Resposta> buscarTodas(){
+    	return respostaRepository.findAll();
     }
 }
