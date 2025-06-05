df8774a4de70b1b9c1768a0bd26f38130d1c864b
CaioLira18 <caiozao1212@gmail.com>
2025-06-05 02:07:02 -0300

    [feat] - tsx to jsx

--- login/src/main.jsx
+++ login/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { Toaster } from 'react-hot-toast'
 import { ChatProvider } from './context/ChatContext.jsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot().render(
   <StrictMode>
     <Toaster position='top-center' />
     <ChatProvider>
